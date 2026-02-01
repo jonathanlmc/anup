b71216322900654e4e1db885b89c4779dc562e67
Jonathan M <jgit@tuta.io>
2026-02-01 04:46:50 -0800

    chore(anup): add comments to the `automatch` module

--- anup/src/series/automatch.rs
+++ anup/src/series/automatch.rs
@@ -15,6 +15,29 @@ pub enum AutomatchResult {
     None(Vec<anime::Anime>),
 }
 
+/// Automatically match all series formats and any continuous seasons within a local
+/// series to one or more anime on a remote service.
+///
+/// ## Continuous Seasons
+///
+/// For each provided series format, this function will detect any episodes belonging
+/// to a separate series / season that are laid out sequentially and add them to a new
+/// season for the format appropriately. This process involves making multiple API calls
+/// to the provided `anime_service`, so this function can take a long time to complete
+/// depending on the number of sequels that need to be analyzed.
+///
+/// For additional context, here is an example of some episode files that contain
+/// a continuous season. Assume each season has 3 episodes:
+///
+/// * `Title - 01.mkv` -> Start of season 1, or S01E01.
+/// * `Title - 02.mkv` -> S01E02
+/// * `Title - 03.mkv` -> S01E03
+/// * `Title - 04.mkv` -> First episode of season 2. In other words, this is now S02E01.
+/// * `Title - 05.mkv` -> S02E02
+/// * `Title - 06.mkv` -> S02E03
+/// * `Title - 07.mkv` -> First episode of season 3, or S03E01`.
+/// * `Title - 08.mkv` -> S03E02
+/// * `Title - 09.mkv` -> S03E03
 pub async fn all_formats_and_seasons<S: anime::api::Service>(
     local_data: anime_detect::TopLevelSeries,
     anime_service: &S,
@@ -61,6 +84,10 @@ struct ScoredFormats {
 }
 
 impl ScoredFormats {
+    /// Score each local series format with all provided anime.
+    ///
+    /// Scores will only be recorded for formats that have a fairly strong (70%)
+    /// similarity to one of the provided anime.
     fn from_searched_anime(
         local_data: anime_detect::TopLevelSeries,
         searched_anime: &HashMap<anime::AnimeID, anime::Anime>,
@@ -75,6 +102,7 @@ impl ScoredFormats {
         let mut format_scores = HashMap::with_capacity(local_data.episodes.len());
 
         for (anime_id, anime) in searched_anime {
+            // compute similarity score between the local name and searched anime titles
             let score = [
                 Some(&anime.title.romaji),
                 Some(&anime.title.native),
@@ -90,6 +118,9 @@ impl ScoredFormats {
             .max()
             .unwrap_or_default();
 
+            // score each series format in the local data by its title & format similarity
+            // to the current anime data
+            //
             // todo: score by season hint as well
             for local_format in local_data.episodes.keys().copied().map(Into::into) {
                 let format_score = if let Some(anime_fmt) = anime.format
@@ -114,6 +145,7 @@ impl ScoredFormats {
                     continue;
                 }
 
+                // store / replace the score the for the format if it's the best one seen yet
                 match format_scores.entry(local_format) {
                     Entry::Occupied(mut entry) => {
                         let (score, media_id) = entry.get_mut();
@@ -145,10 +177,12 @@ impl ScoredFormats {
         }
     }
 
+    /// Returns true if no scores were recorded for any formats.
     fn is_empty(&self) -> bool {
         self.scores.is_empty()
     }
 
+    /// Returns true if there are any local series formats that were not given an adequate score.
     fn any_formats_missing(&self) -> bool {
         self.local_data
             .episodes
@@ -156,6 +190,10 @@ impl ScoredFormats {
             .any(|&local_fmt| !self.scores.contains_key(&local_fmt.into()))
     }
 
+    /// Link each scored format with its best matching anime.
+    ///
+    /// This will resolve any continuous seasons detected in any
+    /// of the local series formats as well, and may take a while to complete.
     async fn pair_with_searched_anime(
         mut self,
         mut searched_anime: HashMap<anime::AnimeID, anime::Anime>,
@@ -217,6 +255,10 @@ struct PairingDetails {
     any_season_pairing_missing: bool,
 }
 
+/// Link a local set of episodes to an anime season, and resolve any
+/// continuous seasons located in the episodes.
+///
+/// All resolved seasons will be inserted into the provided `resolved_seasons` map.
 async fn pair_anime_seasons_from_local_episodes(
     anime_id: anime::AnimeID,
     anime: anime::Anime,
@@ -226,12 +268,18 @@ async fn pair_anime_seasons_from_local_episodes(
 ) -> anyhow::Result<bool> {
     let highest_episode_num = episodes.iter().map(|ep| ep.number).max().unwrap_or(0);
 
+    // calculate an episode offset if the highest episode number exceeds
+    // the provided anime's episode count
+    //
+    // (i.e. there is at least one continuous season in the episodes)
     let episode_offset = anime
         .episodes
         .and_then(|season_eps| (highest_episode_num > season_eps).then_some(season_eps));
 
     let mut season_num = 1;
 
+    // fast path: if no episode offset was calculated, we can assume there is
+    // no continuous season and return all episodes as the first season
     let Some(mut episode_offset) = episode_offset else {
         resolved_seasons.insert(
             season_num,
@@ -245,6 +293,8 @@ async fn pair_anime_seasons_from_local_episodes(
         return Ok(false);
     };
 
+    // otherwise, find all episodes *within* the first season
+    // for the first resolved season
     resolved_seasons.insert(
         season_num,
         FormatData {
@@ -259,6 +309,8 @@ async fn pair_anime_seasons_from_local_episodes(
     let mut any_season_missing = false;
     let mut current_sequel = anime_id;
 
+    // now loop over each sequel to the first series and extract
+    // its episode range into a new season
     while let Some(sequel_id) = anime_service
         .sequel_id(&crate::REQWEST_CLIENT, current_sequel)
         .await?
