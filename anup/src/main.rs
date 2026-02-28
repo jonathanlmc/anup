5be00e4209afe847899b05174a08fe9536dbcda1
Jonathan M <jgit@tuta.io>
2026-02-27 16:26:08 -0800

    perf(anup): parse `RUST_LOG` manually to avoid a dependency on `regex-automata`

--- anup/src/main.rs
+++ anup/src/main.rs
@@ -7,6 +7,7 @@ mod tui;
 use std::sync::LazyLock;
 
 use anyhow::Context;
+use tracing::level_filters::LevelFilter;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 use crate::tui::state;
@@ -15,9 +16,17 @@ static REQWEST_CLIENT: LazyLock<reqwest::Client> = LazyLock::new(reqwest::Client
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    // the `EnvFilter` layer for `tracing_subscriber` can do this for us but
+    // it pulls in the large `regex-automata` crate which is overkill for our
+    // purposes, so parse the env manually
+    let tracing_target = std::env::var("RUST_LOG")
+        .ok()
+        .and_then(|val| val.parse::<tracing_subscriber::filter::Targets>().ok())
+        .unwrap_or(tracing_subscriber::filter::Targets::new().with_default(LevelFilter::ERROR));
+
     tracing_subscriber::registry()
         .with(tracing_subscriber::fmt::layer())
-        .with(tracing_subscriber::EnvFilter::from_default_env())
+        .with(tracing_target)
         .init();
 
     let path = std::env::args().nth(1).context("missing path arg")?;
