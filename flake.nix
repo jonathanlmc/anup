c319dea223efbcd37e5e85041c0a436f5e8eb08e
Jonathan M <jgit@tuta.io>
2026-06-30 16:34:15 -0700

    add `flake.nix`

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "anup devshell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      {
+        devShells.default = with pkgs; mkShell {
+          buildInputs = [
+            pkg-config
+            openssl.dev
+            (rust-bin.stable.latest.default.override {
+              extensions = [ "rust-src" ];
+            })
+          ];
+        };
+      }
+    );
+}
